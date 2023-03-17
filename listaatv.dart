f76530725bc4010514bb79180a747a4c85f11642
cahenq <127959586+cahenq@users.noreply.github.com>
2023-03-17 09:07:11 -0400

    Update listaatv.dart

--- listaatv.dart
+++ listaatv.dart
@@ -9,16 +9,4 @@ void main(){
   }
   lista.removeAt(5);
   print(lista);
-}
-import 'dart:io';
 
-void main(){
-  var lista = [];
-  for (var numeros = 0; numeros < 10; numeros++) {
-    print('Digite um número: ');
-    int numeros = int.parse(stdin.readLineSync()!);
-    lista.add(numeros);
-  }
-  lista.removeAt(5);
-  print(lista);
-}
